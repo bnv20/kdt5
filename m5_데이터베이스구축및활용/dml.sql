2cfe602634659e2900cfacce7de502a8ef3e8e90
bnv20 <79875713+bnv20@users.noreply.github.com>
2022-02-17 13:40:27 +0900

    Add files via upload

--- m5_데이터베이스구축및활용/dml.sql
+++ m5_데이터베이스구축및활용/dml.sql
@@ -120,3 +120,136 @@ FROM CUSTOMER
 WHERE CUSTID IN (SELECT CUSTID FROM ORDERS
 WHERE BOOKID IN (SELECT BOOKID FROM BOOK
 WHERE PUBLISHER = '대한미디어'));
+
+--Q. 출판사별로 출판사의 평균 도서 가격보다 비싼 도서를 구하시오.
+SELECT * FROM BOOK;
+SELECT b1.bookname
+FROM book b1
+WHERE b1.price > (SELECT avg(b2.price)
+FROM book b2 WHERE b2.publisher=b1.publisher);
+
+--Q. 도서를 주문하지 않은 고객의 이름을 보이시오.
+SELECT * FROM customer;
+SELECT * FROM orders;
+SELECT name
+FROM customer
+MINUS
+SELECT name
+FROM customer
+WHERE custid IN(SELECT custid FROM orders);
+
+--Q. 주문이 있는 고객의 이름과 주소를 보이시오.
+SELECT name, address
+FROM customer
+WHERE custid IN(SELECT custid FROM orders);
+
+--Q.Customer 테이블에서 고객번호가 5인 고객의 주소를 ‘대한민국 부산’으로 변경하시오.
+UPDATE customer
+SET address = '대한민국 부산'
+WHERE custid=5;
+
+SELECT * FROM customer;
+--Q.Customer 테이블에서 박세리 고객의 주소를 김연아 고객의 주소로 변경하시오.
+UPDATE customer
+SET address = (SELECT address FROM customer
+WHERE name = '김연아')
+WHERE name = '박세리';
+SELECT * FROM customer;
+--Q.Customer 테이블에서 고객번호가 5인 고객을 삭제한 후 결과를 확인하시오.
+DELETE customer
+WHERE custid=5;
+
+SELECT ABS(-78), ABS(+78) FROM DUAL;
+SELECT ROUND(4.875,1) FROM DUAL;
+SELECT * FROM orders;
+--Q.고객별 평균 주문 금액을 백원 단위로 반올림한 값을 구하시오.
+select custid 고객번호,round(avg(saleprice),-2) "평균 주문 금액"
+from orders
+group by custid;
+
+--Q.도서 제목에 ‘야구’가 포함된 도서를 ‘농구’로 변경한 후 도서 목록, 가격을 보이시오.
+SELECT * FROM book;
+SELECT bookid, REPLACE(bookname, '야구','농구') bookname, price
+FROM book;
+
+--Q.‘굿스포츠’에서 출판한 도서의 제목과 제목의 글자 수, 바이트 수를 보이시오.
+SELECT bookname 제목, LENGTH(bookname) 글자수, LENGTHB(bookname) 바이트수
+FROM book
+WHERE publisher = '굿스포츠';
+
+SELECT * FROM customer;
+INSERT INTO customer VALUES(6,'박세리','대한민국 대전','000-9000-0001');
+INSERT INTO customer VALUES(5,'박세리','대한민국 대전',null);
+DELETE customer
+WHERE custid = 6;
+
+--Q.마당서점의 고객 중에서 같은 성(姓)을 가진 사람이 몇 명이나 되는지 성별 인원수를 구하시오.
+SELECT SUBSTR(name,1,1)성,COUNT(*)인원수
+FROM customer
+GROUP BY SUBSTR(name,1,1);
+
+--Q. 마당서점은 주문일로 부터 10일 후 매출을 확정한다. 각 주문의 확정일자를 구하세요.
+SELECT orderdate 주문일자, orderdate+10 확정일자
+FROM orders;
+
+--Q.DBMS 서버에 설정된 현재 날짜와 시간, 요일을 확인하시오. 
+SELECT SYSDATE FROM DUAL;
+SELECT SYSDATE, TO_CHAR(SYSDATE,'yyyy/mm/dd dy hh24:mi:ss')SYSDATE1
+FROM dual;
+
+--Q.마당서점이 2020년 7월 7일에 주문받은 도서의 주문번호, 주문일, 고객번호, 도서번호를 모두 보이시오.
+SELECT * FROM orders;
+SELECT orderid 주문번호,TO_CHAR(orderdate,'YYYY-mm-dd day') 주문일,custid 고객번호, bookid 도서번호
+FROM orders
+WHERE orderdate = '20/07/07';
+ 
+--Q.고객목록에서 고객번호, 이름, 전화번호를 앞의 두 명만 보이시오.
+SELECT * FROM customer;
+SELECT ROWNUM 순번, custid 고객번호, name 이름, phone 전화번호
+FROM customer
+WHERE ROWNUM<=2;
+
+--Q.평균 주문금액 이하의 주문에 대해서 주문번호와 금액을 보이시오.
+SELECT orderid,saleprice FROM orders
+WHERE saleprice <= (SELECT AVG(saleprice) FROM orders);
+
+--Q.전체 평균 주문금액보다 큰 금액의 고객 주문 내역에 대해서 주문번호, 고객번호, 금액을 보이시오.
+SELECT * FROM orders;
+SELECT	orderid, custid, saleprice
+from Orders b1
+where b1.saleprice > (select avg(b2.saleprice)
+from Orders b2 where b2.custid = b1.custid);
+
+--Q.‘대한민국’에 거주하는 고객에게 판매한 도서의 총 판매액을 구하시오.
+SELECT * FROM customer;
+SELECT SUM(saleprice) 총판매액
+FROM orders
+WHERE custid IN (SELECT custid FROM customer WHERE address LIKE '%대한민국%');
+
+--Q.3번 고객이 주문한 도서의 최고 금액보다 더 비싼 도서를 구입한 주문의 주문번호와 금액을 보이시오.
+select orderid, saleprice from orders 
+where saleprice > (select max(saleprice) from orders where custid='3');
+
+--[과제]EXISTS 연산자를 사용하여 ‘대한민국’에 거주하는 고객에게 판매한 도서의 총 판매액을 구하시오.
+SELECT SUM(saleprice)total
+FROM orders o
+WHERE EXISTS(SELECT * FROM customer c 
+WHERE address LIKE'%대한민국%' AND o.custid=c.custid);
+
+--[과제]마당서점의 고객별 판매액을 보이시오(고객이름과 고객별 판매액 출력)
+SELECT (SELECT name FROM customer c WHERE c.custid=o.custid)name,
+SUM(saleprice) total FROM orders o
+GROUP BY o.custid;
+
+--[과제] 고객번호가 2 이하인 고객의 판매액을 보이시오(고객이름과 고객별 판매액 출력)
+SELECT * FROM customer;
+SELECT c.name, SUM(o.saleprice) total
+FROM(SELECT custid, name
+FROM customer
+WHERE custid<=2) c,
+orders o
+WHERE c.custid = o.custid
+GROUP BY c.name;
+
+
+
